508d0b7954ab618c525352e92d021843a5cf6133
Diouani <diouanijalouka@gmail.com>
2023-09-05 11:24:09 +0200

    modified the .gitignore

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,8 @@ export default defineConfig({
   build: {
     outDir: "dist", // Update this to 'dist'
   },
+  publicDir: "dist", // Specify the directory containing your static assets (CSS files)
+  css: {
+    modules: true, // Disable CSS modules unless you need them
+  },
 });
